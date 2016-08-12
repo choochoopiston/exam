270c7415c50abcad5886e9535480632dfd4f2515
choochoopiston <choochoopiston@gmail.com>
2016-08-12 15:11:04 +0000

    画像を移動とシンボリックファイル

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,11 +14,10 @@ set :branch, 'master'
 set :deploy_to, '/var/www/achieve'
 
 # シンボリックリンクをはるファイル。(※後述)
-set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
-set :linked_files, %w{ config/database.yml config/secrets.yml }
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml', ".env")
 
 # シンボリックリンクをはるフォルダ。(※後述)
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # 保持するバージョンの個数(※後述)
 set :keep_releases, 5
@@ -34,6 +33,7 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
   desc 'Create database'
   task :db_create do
     on roles(:db) do |host|
@@ -44,6 +44,7 @@ namespace :deploy do
       end
     end
   end
+
   desc 'Run seed'
   task :seed do
     on roles(:app) do
@@ -54,23 +55,11 @@ namespace :deploy do
       end
     end
   end
+
   after :publishing, :restart
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
     end
   end
-  task :copy_config do
-    on release_roles :app do |role|
-      fetch(:linked_files).each do |linked_file|
-        user = role.user + "@" if role.user
-        hostname = role.hostname
-        linked_files(shared_path).each do |file|
-          run_locally do
-            execute :rsync, "config/#{file.to_s.gsub(/.*\/(.*)$/,"\\1")}", "#{user}#{hostname}:#{file.to_s.gsub(/(.*)\/[^\/]*$/, "\\1")}/"
-          end
-        end
-      end
-    end
-  end
 end
-before "deploy:check:linked_files", "deploy:copy_config"
