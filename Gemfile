cb3c3fd9887942e343ed7a979b69293b44b5b9af
choochoopiston <choochoopiston@gmail.com>
2016-08-12 14:53:27 +0000

    capistrano ng1a

--- Gemfile
+++ Gemfile
@@ -67,10 +67,10 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 
-  gem 'capistrano', '3.6.0'
-  gem 'capistrano-bundler'
+  gem 'capistrano', '3.4.0'
   gem 'capistrano-rails'
   gem 'capistrano-rbenv'
+  gem 'capistrano-bundler'
   gem 'capistrano3-unicorn'
 end
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,50 +1,24 @@
-# config valid only for current version of Capistrano
-lock '3.6.0'
+# capistranoのバージョン固定
+lock '3.4.0'
 
-set :application, 'exam'
+# デプロイするアプリケーション名
+set :application, 'achieve'
 
 # cloneするgitのレポジトリ
 set :repo_url, ENV['REPO_URL']
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, ENV['BRANCH'] || 'master'
+# deployするブランチ。デフォルトはmasterなのでなくても可。
+set :branch, 'master'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-set :deploy_to, '/var/www/exam'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# deploy先のディレクトリ。
+set :deploy_to, '/var/www/achieve'
 
 # シンボリックリンクをはるファイル。(※後述)
-set :linked_files, fetch(:linked_files, []).push('config/secrets.yml', ".env")
+set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 set :linked_files, %w{ config/database.yml config/secrets.yml }
 
 # シンボリックリンクをはるフォルダ。(※後述)
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # 保持するバージョンの個数(※後述)
 set :keep_releases, 5
@@ -60,7 +34,6 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-
   desc 'Create database'
   task :db_create do
     on roles(:db) do |host|
@@ -71,7 +44,6 @@ namespace :deploy do
       end
     end
   end
-
   desc 'Run seed'
   task :seed do
     on roles(:app) do
@@ -82,11 +54,23 @@ namespace :deploy do
       end
     end
   end
-
   after :publishing, :restart
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
     end
   end
+  task :copy_config do
+    on release_roles :app do |role|
+      fetch(:linked_files).each do |linked_file|
+        user = role.user + "@" if role.user
+        hostname = role.hostname
+        linked_files(shared_path).each do |file|
+          run_locally do
+            execute :rsync, "config/#{file.to_s.gsub(/.*\/(.*)$/,"\\1")}", "#{user}#{hostname}:#{file.to_s.gsub(/(.*)\/[^\/]*$/, "\\1")}/"
+          end
+        end
+      end
+    end
+  end
 end
+before "deploy:check:linked_files", "deploy:copy_config"
